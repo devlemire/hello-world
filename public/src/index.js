a2135509ba4ba2c038f18eb29ce05e0a46edd3a2
= <webdev.jameslemire@gmail.com>
2017-08-15 12:34:35 -0400

    JL master - Move server.

--- public/src/index.js
+++ public/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+import axios from 'axios';
+axios.defaults.baseURL = 'https://jameslemire.me/projects/hello-world';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const config = require('./config');
 const app = express();
 
 app.use( cors() );
-app.use( express.static(`${__dirname}/../public/build`) ); 
+app.use( express.static(`${__dirname}/public/build`) ); 
 
-app.get('/api/test', ( req, res, next ) => {
-  res.status(200).send('Super Secret Message');
+app.get('/api/test/2', ( req, res, next ) => {
+  res.status(200).send('Super Secret Message 2');
 });
 
 app.listen( config.port || 3000, () => { console.log(`App listening on port ${ config.port || 3000 }`); } );
